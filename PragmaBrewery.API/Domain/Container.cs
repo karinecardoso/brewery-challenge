a3dd55c08b3084699b527544b6b2d71197c6be43
Karine Cardoso <karine.cardoso_@hotmail.com>
2019-05-13 23:42:08 -0300

    Add beer and container domains

---
+++ PragmaBrewery.API/Domain/Container.cs
@@ -0,0 +1,28 @@
+﻿namespace PragmaBrewery.API.Domain
+{
+    public class Container
+    {
+        public Container()
+        {
+        }
+
+        public Container(string name, long idBeer)
+        {
+            Name = name;
+            IdBeer = idBeer;
+        }
+
+        public Container(long id, string name, long idBeer)
+        {
+            Id = id;
+            Name = name;
+            IdBeer = idBeer;
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long IdBeer { get; set; }
+
+        public Beer Beer { get; set; }
+    }
+}
