d7a2845f147eecf524d4fe21e00d4e2e20dba587
Karine Cardoso <karine.cardoso_@hotmail.com>
2019-05-13 23:59:18 -0300

    Add beer and container controllers

---
+++ PragmaBrewery.API/Controllers/BeersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PragmaBrewery.API.Contracts.Beers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PragmaBrewery.API.Controllers
+{
+    [Route("beers")]
+    public class BeersController : Controller
+    {
+        private readonly IBeerService _beerService;
+
+        public BeersController(IBeerService beerService)
+        {
+            _beerService = beerService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BeerDto>>> GetBeers()
+        {
+            return await _beerService.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BeerDto>> GetBeer(int id)
+        {
+            var beer = await _beerService.GetById(id);
+
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            return beer;
+        }
+
+        [HttpGet("containers-temperatures")]
+        public async Task<ActionResult<IEnumerable<BeerContainerDto>>> GetBeersContainersTemperatures()
+        {
+            return await _beerService.GetContainersTemperatures();
+        }
+    }
+}
