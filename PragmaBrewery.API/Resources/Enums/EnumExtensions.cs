712190f9657a4ab29055e19c8a76a3958d211fb8
Karine Cardoso <karine.cardoso_@hotmail.com>
2019-05-13 23:36:31 -0300

    Add BeerType, TemperatureAlertType and EnumExtensions

---
+++ PragmaBrewery.API/Resources/Enums/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace PragmaBrewery.API.Resources.Enums
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            return value
+                .GetType()
+                .GetMember(value.ToString())
+                .FirstOrDefault()
+                ?.GetCustomAttribute<DescriptionAttribute>()
+                ?.Description;
+        }
+    }
+}
